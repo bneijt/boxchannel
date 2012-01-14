39ebf45997d5aa3a2a4d81655e7595b071932c6a
A. Bram Neijt <bneijt@gmail.com>
2012-01-14 16:19:09 +0100

    Remove main function from boxchannel.py

--- src/boxchannel.py
+++ src/boxchannel.py
@@ -77,34 +77,3 @@ def saveUserPreferences(prefs):
     pf.close()
 
 
-
-def main():
-    logging.basicConfig(level=logging.INFO, format='%(message)s')
-    knownBlocks = {}
-    #Load database
-    for fileName in sys.argv[1:]:
-        logging.info("Loading local block info from: %s", fileName)
-        of = file(fileName, 'r')
-        idx = 0
-        for blockHash in hashesFor(of):
-            knownBlocks[blockHash] = LocalBlock(fileName, idx)
-            logging.info("Loaded %s", blockHash)
-            idx += 1
-    logging.info("Loaded %i blocks", len(knownBlocks))
-    
-    
-    #Respond to requests
-    for requestFile in glob.glob("request/*"):
-        logging.info("Found request: %s", requestFile)
-        requestHash = requestFile.split(".", 1)[0]
-        if requestHash in knownBlocks:
-            publishBlock(knownBlocks[requestHash])
-    logging.info("EXIT")
-    #cleanRequests()
-    #cleanResponses()
-    
-    
-
-
-if __name__ == "__main__":
-    main()
